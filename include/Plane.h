67dde03066107f1caa75d501978cc1cb5fd7f3dd
Marcin Stramowski <stramekz@gmail.com>
2017-02-11 19:29:35 +0100

    Plane model
    Plane model

---
+++ include/Plane.h
@@ -0,0 +1,32 @@
+//
+//  Plane.hpp
+//  ProjektMagisterski
+//
+//  Created by Marcin Stramowski on 11.02.2017.
+//
+//
+
+#ifndef Plane_h
+#define Plane_h
+#ifdef __GNUC__
+// Avoid tons of warnings with root code
+#pragma GCC system_header
+#endif
+
+#include <opencv2/opencv.hpp>
+#include <array>
+
+class Plane {
+public:
+    Plane();
+    Plane(cv::Point3d point1, cv::Point3d point2, cv::Point3d point3);
+    Plane(std::array<cv::Point3d, 3>);
+    double getA();
+    double getB();
+    double getD();
+    double getDistanceFromPoint(cv::Point3d point);
+private:
+    double A, B, D;
+};
+
+#endif /* Plane_h */
