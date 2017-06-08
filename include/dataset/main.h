f0053d641ef46278a342a57853da44382e6487d2
Marcin Stramowski <stramekz@gmail.com>
2017-06-08 14:39:29 +0200

    #39 working DA

--- include/dataset/main.h
+++ include/dataset/main.h
@@ -13,31 +13,5 @@
 #pragma GCC system_header
 #endif
 
-#include <iostream>
-#include <QGLViewer/qglviewer.h>
-#include <qapplication.h>
-#include <GL/glut.h>
-#include "include/utils/Qvisualizer.h"
-#include "include/utils/constants.h"
-#include "include/models/ImageCoords.h"
-#include <opencv2/opencv.hpp>
-#include "include/models/ImageLoader.h"
-#include <rxcpp/rx.hpp>
-#include <include/utils/utils.h>
-#include <include/algorithms/PcaPlaneDetector.h>
-#include "include/utils/planeUtils.h"
-#include "include/models/PlaneFiller.h"
-#include "include/algorithms/RansacPlaneDetector.h"
-
-namespace Rx {
-    using namespace rxcpp;
-    using namespace rxcpp::sources;
-    using namespace rxcpp::operators;
-    using namespace rxcpp::util;
-}
-
-using namespace Rx;
-using namespace std;
-using namespace cv;
 
 #endif
