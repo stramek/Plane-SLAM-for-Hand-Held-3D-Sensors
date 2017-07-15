6dff18f35b4fba85fe38dadb49d42f21bc26a04d
Marcin Stramowski <stramekz@gmail.com>
2017-07-15 15:41:03 +0200

    PosOrient object

---
+++ include/models/PosOrient.h
@@ -0,0 +1,25 @@
+//
+// Created by stramek on 15.07.17.
+//
+
+#ifndef PROJEKTMAGISTERSKI_POSORIENT_H
+#define PROJEKTMAGISTERSKI_POSORIENT_H
+
+#include <Eigen/Dense>
+#include <iostream>
+#include "math.h"
+
+using namespace Eigen;
+using namespace std;
+
+class PosOrient {
+public:
+    PosOrient(const Vector3d &position, const Vector4d &orientation);
+    void print();
+    void printDiff(const PosOrient &posOrient);
+private:
+    Vector3d position;
+    Vector4d orientation;
+};
+
+#endif //PROJEKTMAGISTERSKI_POSORIENT_H
---
+++ src/models/PosOrient.cpp
@@ -0,0 +1,26 @@
+//
+// Created by stramek on 15.07.17.
+//
+
+#include "include/models/PosOrient.h"
+
+
+PosOrient::PosOrient(const Vector3d &position, const Vector4d &orientation) : position(position),
+                                                                              orientation(orientation) {
+
+}
+
+void PosOrient::print() {
+    cout<<"x: "<<position[0]<<" y: "<<position[1]<<" z: "<<position[2]<<endl;
+    cout<<"q1: "<<orientation[0]<<" q2: "<<orientation[1]<<" q3: "<<orientation[2]<<" q4: "<<orientation[3];
+}
+
+void PosOrient::printDiff(const PosOrient &posOrient) {
+    cout<<"x: "<<abs(position[0] - posOrient.position[0])
+        <<" y: "<<abs(position[1] - posOrient.position[1])
+        <<" z: "<<abs(position[2] - posOrient.position[2])<<endl;
+    cout<<"q1: "<<abs(orientation[0] - posOrient.orientation[0])
+        <<" q2: "<<abs(orientation[1] - posOrient.orientation[1])
+        <<" q3: "<<abs(orientation[2] - posOrient.orientation[2])
+        <<" q4: "<<abs(orientation[3] - posOrient.orientation[3])<<endl;
+}
